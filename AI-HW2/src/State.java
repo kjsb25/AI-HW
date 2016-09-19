94c67ec684cf9f5a396ec6e5be285a704b01223c
Keenan Shumard <keenan.shumard@gmail.com>
2016-09-18 22:41:46 -0500

    Did some more work on State

--- AI-HW2/src/State.java
+++ AI-HW2/src/State.java
@@ -1,22 +1,49 @@
+import java.util.ArrayList;
 
 public class State {
-	private Room[][] layout;
-	private int vacuumX;
-	private int vacuumY;
+	private static int[][] layout;
+	private static int maxX;
+	private static int maxY;
+	private static int vacuumX;
+	private static int vacuumY;
 	
 	
-	public State(Room[][] layout, int vacuumX, int vacuumY) {
+	public State(int maxX,int maxY,ArrayList<DirtyIndex> dirty, int vacuumX, int vacuumY) {
 		super();
-		this.layout = layout;
+		setLayout(maxX,maxY,dirty);
+		this.maxX=maxX;
+		this.maxY=maxY;
 		this.vacuumX = vacuumX;
 		this.vacuumY = vacuumY;
 	}
-	public Room[][] getLayout() {
+	
+	public int[][] getLayout() {
 		return layout;
 	}
-	public void setLayout(Room[][] layout) {
+
+	public void setLayout(int maxX, int maxY,ArrayList<DirtyIndex> dirtyList) {
+		int[][] layout=new int[maxX][maxY];
+		
+		for(DirtyIndex hold: dirtyList){
+			layout[hold.x][hold.y]=1;
+		}
 		this.layout = layout;
 	}
+	
+	public static void printRooms(){
+		System.out.println("Room:");
+		for(int i=0;i<maxX;i++){
+			for(int j=0;j<maxY;j++){
+				if(layout[i][j]==0){
+					System.out.print("[ ]");
+				}else{
+					System.out.print("[*]");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+
 	public int getVacuumX() {
 		return vacuumX;
 	}
@@ -29,4 +56,6 @@ public class State {
 	public void setVacuumY(int vacuumY) {
 		this.vacuumY = vacuumY;
 	}
+	
+	
 }
