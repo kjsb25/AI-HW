f06d97dd3e106b3bad712bbc5c1b47de47c8e546
Keenan Shumard <keenan.shumard@gmail.com>
2016-09-18 22:17:15 -0500

    Finalized the structure for how the tree is gonna work, now on to implementation

--- AI-HW2/src/Tree.java
+++ AI-HW2/src/Tree.java
@@ -1,22 +1,21 @@
-import org.w3c.dom.Node;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Tree<State> {
 	
 	private Node<State> root;
-	private State state;
-	
-	public Tree(State rootData) {
-        root = new Node<State>();
-        root.data = rootData;
-        root.children = new ArrayList<Node<State>>();
-    }
+
+	 public Tree(State rootData) {
+	        root = new Node<State>();
+	        root.data = rootData;
+	        root.children = new ArrayList<Tree<State>>();
+	}
 
     public static class Node<State> {
         private State data;
-        private Node<State> parent;
-        private List<Node<State>> children;
+        private Tree<State> parent;
+        private List<Tree<State>> children;
     }
 	
 }
