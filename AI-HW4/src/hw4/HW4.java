5de1484f40e13183cdfd5093e7cb0217cab9f965
Keenan Shumard <keenan.shumard@gmail.com>
2016-10-04 13:08:06 -0500

    Couple more edits
    Also a note for later, when I can actually start implementing this.

--- AI-HW4/src/hw4/HW4.java
+++ AI-HW4/src/hw4/HW4.java
@@ -6,5 +6,7 @@ public class HW4 {
 		// TODO Auto-generated method stub
 
 	}
+	
+	//may just implement players here, while they are technically objects it just seems easier to implement them as functions....
 
 }
---
+++ AI-HW4/src/hw4/State.java
@@ -0,0 +1,20 @@
+package hw4;
+
+public class State {
+	public char[][] board;
+
+	public State(char[][] board) {
+		super();
+		this.board = board;
+	}
+
+	public char[][] getBoard() {
+		return board;
+	}
+
+	public void setBoard(char[][] board) {
+		this.board = board;
+	}
+	
+
+}
