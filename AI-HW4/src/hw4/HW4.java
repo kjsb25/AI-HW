f9a73e1bc8b561e1df4bb852c6b3f2e28cc05544
Keenan Shumard <keenan.shumard@gmail.com>
2016-10-04 13:47:15 -0500

    Added State Functionality
    Added functions to interact with board in a state, complete with error
    checking. PrintBoard, setting a position, and checking a position.
    Hopefully using this API, we can get around the off by one errors that
    are usually a hassle with arrays.

--- AI-HW4/src/hw4/HW4.java
+++ AI-HW4/src/hw4/HW4.java
@@ -3,10 +3,29 @@ package hw4;
 public class HW4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		State startingState=new State();
+		startingState.printBoard();
+		startingState.markPosition(2, 4, 'X');
+		startingState.markPosition(1, 1, 'O');
+		startingState.printBoard();
 	}
 	
 	//may just implement players here, while they are technically objects it just seems easier to implement them as functions....
+	
+	public static State BeginnerDecision(State state){
+		State newState=new State(state.getBoard());
+		
+		return newState;
+	}
+	
+	/**
+	 * Find if there is an open end for two in a row for the symbol, and returns coordinates of that position
+	 * 
+	 * @param symbol
+	 * @param board
+	 */
+	public static void isTwoInARowOpen(char symbol, State board){
+		
+	}
 
 }
--- AI-HW4/src/hw4/State.java
+++ AI-HW4/src/hw4/State.java
@@ -1,13 +1,68 @@
 package hw4;
 
+
 public class State {
 	public char[][] board;
+	static final int BoardLength=4;
 
+	public State() {
+		super();
+		this.board = buildStartBoard();
+	}
+	
 	public State(char[][] board) {
 		super();
 		this.board = board;
 	}
+	
+	public char[][] buildStartBoard(){
+		char[][] board=new char[BoardLength][BoardLength];
+		for(int i=0;i<BoardLength;i++){
+			for(int j=0;j<BoardLength;j++){
+				board[i][j]=' ';
+			}
+		}
+		return board;
+	}
+	
+	public void printBoard(){
+		System.out.print("Board:");
+		for(int i=0;i<BoardLength;i++){
+			System.out.print("\n");
+			for(int j=0;j<BoardLength;j++){
+				System.out.print("["+board[i][j]+"]");
+			}
+		}
+		System.out.println("");
+	}
+	
+	public char valueAtPos(int x, int y){
+		if(x>BoardLength || x<=0 || y>BoardLength || y<=0){
+			System.out.print("Invalid position passed to isPosMarked");
+		}
+		x--;
+		y--;
+		return board[x][y];
+	}
 
+	public boolean markPosition(int x, int y,char symbol){
+		if(x>BoardLength || x<=0 || y>BoardLength || y<=0){
+			System.out.print("Invalid position passed to markPosition");
+			return false;
+		}
+		if(symbol!='X' && symbol!='O'){
+			System.out.print("Invalid symbol passed to markPosition");
+			return false;
+		}
+		x--;
+		y--;
+		if(' '!=board[x][y]){
+			return false;
+		}
+		board[x][y]=symbol;
+		return true;
+	}
+	
 	public char[][] getBoard() {
 		return board;
 	}
@@ -15,6 +70,8 @@ public class State {
 	public void setBoard(char[][] board) {
 		this.board = board;
 	}
+	
+	
 	
 
 }
