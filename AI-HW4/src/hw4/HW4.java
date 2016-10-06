e369caf98fd84241976dfd71b9ed1ef3f9f15abf
Keenan Shumard <keenan.shumard@gmail.com>
2016-10-05 20:14:33 -0500

    User turn function works
    Yeah Bitches

--- AI-HW4/src/hw4/HW4.java
+++ AI-HW4/src/hw4/HW4.java
@@ -1,12 +1,15 @@
 package hw4;
 
+import java.util.Scanner;
+
 public class HW4 {
 
 	public static void main(String[] args) {
 		State startingState=new State();
 		startingState.printBoard();
-		startingState.markPosition(2, 4, 'X');
-		startingState.markPosition(1, 1, 'O');
+		char player='X';
+		userDecision(startingState,player);
+		
 		startingState.printBoard();
 	}
 	
@@ -18,11 +21,42 @@ public class HW4 {
 		return newState;
 	}
 	
-	public static boolean userDecision(char symbol){
+	public static boolean userDecision(State state,char symbol){
+		//input chekcing
 		if(symbol!='O' && symbol!='X'){
 			return false;
 		}
-		System.out.print("Select a coordinate where you would like ");
+		//create new reader for input, and testing boolean
+		Scanner reader = new Scanner(System.in);
+		boolean input_valid=false;
+		
+		//while a move hasnt been made
+		while(!input_valid){
+			//print prompt and scan input
+			System.out.print("Select x and y coordinates:");
+			String input_string=reader.nextLine();
+			Scanner input=new Scanner(input_string);
+			
+			//check for validity
+			if(input.hasNextInt()){
+				int x=input.nextInt();
+				if(input.hasNextInt()){
+					int y=input.nextInt();
+					//attempt to set that position
+					if(state.markPosition(x, y, symbol)){
+						input_valid=true;
+					}else{
+						System.out.println("Input was outside of board. Try again.");
+					}
+				}else{
+					System.out.println("Y is not a number. Try again.");
+				}
+			}else{
+				System.out.println("X is not a number. Try again.");
+			}
+			input.close();
+		}
+		reader.close();
 		return true;
 	}
 	
